11724e1da8e7c54525c5b776bd55d99726a0cf88
Beth Jackson <beth.jack@gmail.com>
2020-12-10 22:57:25 +0100

    Finalised fastest presser

--- javascript/javascript2/week3/fastest-presser.js
+++ javascript/javascript2/week3/fastest-presser.js
@@ -1,22 +1,37 @@
-let game;
-let countS = 0;
-let countL = 0;
-let spanS = document.getElementById("count-s");
-let spanL = document.getElementById("count-l");
-let timer = document.getElementById("timer");
+let game; // interval
+let countS = 0; // playerOne
+let countL = 0; // playerTwo
+const spanS = document.getElementById("count-s"); // playerOneScoreElement
+const spanL = document.getElementById("count-l");
+const timer = document.getElementById("timer"); // timerInput
 
-const confettiS = new ConfettiGenerator({
-  target: "my-canvas-s",
-  width: 300,
-  height: 300,
-});
-const confettiL = new ConfettiGenerator({
-  target: "my-canvas-l",
-  width: 300,
-  height: 300,
-});
+const rematch = document.getElementById("button"); // buttonElement
+rematch.textContent = "Start game!";
+const message = document.getElementById("message");
+message.textContent = "Set seconds and start game by clicking on the button.";
+
+function createConfetti(id) {
+  const canvas = document.createElement("canvas");
+  canvas.setAttribute("id", "my-canvas");
+  document.getElementById(id).appendChild(canvas);
+  const confetti = new ConfettiGenerator({
+    target: "my-canvas",
+    width: 300,
+    height: 300,
+  });
+  confetti.render();
+}
+
+function removeConfetti() {
+  const canvas = document.getElementById("my-canvas");
+  if (canvas === null) {
+    return;
+  }
+  canvas.parentNode.removeChild(canvas);
+}
 
 function count(e) {
+  //updatePlayerScore or onKeyDown
   if (e.key === "s") {
     countS++;
   } else if (e.key === "l") {
@@ -29,37 +44,66 @@ function count(e) {
 
 function indicateWinner() {
   if (countS > countL) {
-    confettiS.render();
+    createConfetti("player-s");
+    spanS.textContent = `${countS}`;
+    spanL.textContent = `${countL}`;
+    message.textContent = "S is the winner!";
   } else if (countL > countS) {
-    confettiL.render();
+    createConfetti("player-l");
+    spanS.textContent = `${countS}`;
+    spanL.textContent = `${countL}`;
+    message.textContent = "L is the winner!";
   } else {
-    console.log("No winner");
+    message.textContent = "Draw! Try a rematch.";
   }
 }
 
+function gameEnd() {
+  //endGame
+  document.removeEventListener("keydown", count);
+  clearInterval(game);
+  indicateWinner();
+
+  //console.log("Game end");
+  countS = 0;
+  countL = 0;
+
+  rematch.textContent = "Rematch!";
+}
+
 function gameStart() {
+  // startGame
+  // console.log("Surprise!");
   spanS.textContent = "";
   spanL.textContent = "";
 
-  const gameLength = Number(input.value) * 1000;
+  removeConfetti();
+
+  message.textContent = "Who is the fastest presser?!";
+
+  const gameLength = Number(timer.value);
+  if (gameLength === 0 || gameLength === undefined) {
+    message.textContent = "Set seconds and start game.";
+    return;
+  }
 
   document.addEventListener("keydown", count);
 
-  game = setTimeout(() => {
-    document.removeEventListener("keydown", count);
-    spanS.textContent = `${countS}`;
-    spanL.textContent = `${countL}`;
-
-    indicateWinner();
-
-    //console.log("Game end");
-    countS = 0;
-    countL = 0;
-    timer.textContent = 0; // PROBLEM
-  }, gameLength);
+  game = setInterval(() => {
+    timer.value = Number(timer.value) - 1;
+    if (timer.value === "3") {
+      message.textContent = "THREE!!!";
+    }
+    if (timer.value === "2") {
+      message.textContent = "TWO!!!";
+    }
+    if (timer.value === "1") {
+      message.textContent = "ONE!!!";
+    }
+    if (timer.value === "0") {
+      gameEnd();
+    }
+  }, 1000);
 }
 
-const startButton = document.querySelector("button");
-startButton.addEventListener("click", gameStart);
-
-const input = document.querySelector("input");
+rematch.addEventListener("click", gameStart);
