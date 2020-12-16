6a2636225a7eb140fb113140bec257673cd705fb
Beth Jackson <beth.jack@gmail.com>
2020-12-16 09:17:13 +0100

    Updates to weather app - nearly finished

--- javascript/javascript3/week1/weather.js
+++ javascript/javascript3/week1/weather.js
@@ -5,7 +5,6 @@ let longitude = 0;
 let url = "";
 
 const cityInput = document.getElementById("city-input");
-const cityName = cityInput.value;
 const buttonElement = document.getElementById("button");
 const messageElement = document.getElementById("message");
 const cityNameElement = document.getElementById("displayed-city-name");
@@ -16,6 +15,12 @@ const cloudyElement = document.getElementById("cloudy");
 const sunriseElement = document.getElementById("sunrise");
 const sunsetElement = document.getElementById("sunset");
 
+const cityNameForecastElement = document.getElementById("forecast-city-name");
+const tempForecastElement = document.getElementById("forecast-temp");
+const iconForecastElement = document.getElementById("forecast-icon");
+const windForecastElement = document.getElementById("forecast-wind-speed");
+const cloudyForecastElement = document.getElementById("forecast-cloudy");
+
 function findCurrentLocation() {
   function success(position) {
     latitude = position.coords.latitude;
@@ -23,6 +28,8 @@ function findCurrentLocation() {
     messageElement.textContent = `Latitude: ${Math.round(
       latitude
     )}, Longitude: ${Math.round(longitude)}`;
+    fetchWeather();
+    fetchForecast();
   }
 
   function error() {
@@ -42,15 +49,16 @@ function fetchWeather() {
   if (cityInput.value === "") {
     url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}${toCelsius}`;
   } else {
-    url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}${toCelsius}`;
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}${toCelsius}`;
   }
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       cityNameElement.textContent = `City: ${data.name}`;
-      tempElement.textContent = `Temperature: ${data.main.temp} celsius`;
-      const iconID = data.weather.icon;
-      iconElement.src = `http://openweathermap.org/img/wn/${iconID}@2x.png`;
+      tempElement.textContent = `Temperature: ${Math.round(
+        data.main.temp
+      )} celsius`;
+      iconElement.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
       windElement.textContent = `Wind speed: ${data.wind.speed} meter/sec`;
       cloudyElement.textContent = `Cloudiness: ${data.clouds.all}%`;
       const riseMilliseconds = data.sys.sunrise * 1000;
@@ -66,6 +74,29 @@ function fetchWeather() {
       console.error(error);
     });
 }
-fetchWeather();
+
+function fetchForecast() {
+  if (cityInput.value === "") {
+    url = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}${toCelsius}`;
+  } else {
+    url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput.value}&appid=${apiKey}${toCelsius}`;
+  }
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      cityNameForecastElement.textContent = `24 hour Forecast: ${data.city.name}`;
+      tempForecastElement.textContent = `Temperature: ${Math.round(
+        data.list[8].main.temp
+      )} celsius`;
+      iconForecastElement.src = `http://openweathermap.org/img/wn/${data.list[8].weather[0].icon}@2x.png`;
+      windForecastElement.textContent = `Wind speed: ${data.list[8].wind.speed} meter/sec`;
+      cloudyForecastElement.textContent = `Cloudiness: ${data.list[8].clouds.all}%`;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
 buttonElement.addEventListener("click", fetchWeather);
+buttonElement.addEventListener("click", fetchForecast);
