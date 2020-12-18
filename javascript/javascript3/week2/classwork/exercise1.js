6bd35c3899b979667240110d416aa6b3f8ff44fc
Beth Jackson <beth.jack@gmail.com>
2020-12-18 10:09:20 +0100

    Started work on class exercises

--- javascript/javascript3/week2/classwork/exercise1.js
+++ javascript/javascript3/week2/classwork/exercise1.js
@@ -1,3 +1,22 @@
 // Using async await fetch yes or no from this api: https://yesno.wtf/api. log out the answer
 
 const url = "https://yesno.wtf/api";
+
+async function fetchYesNo() {
+
+let promise = fetch(url).then((response) => ).then().catch();
+
+
+
+
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve("done!"), 1000)
+    });
+  
+    let result = await promise; // wait until the promise resolves (*)
+  
+    alert(result); // "done!"
+  }
+  
+  fetchYesNo();
+
