efa147c1c41d6970ecef5304761dab8c64c1f806
Beth Jackson <beth.jack@gmail.com>
2021-02-18 13:40:46 +0100

    Completed get part of warmup

--- nodejs/week3/warmup/index.js
+++ nodejs/week3/warmup/index.js
@@ -5,54 +5,79 @@ const router = express.Router();
 
 // app.get("/", (request, response) => response.send("nodejs week3 homework"));
 
-router.use("/add", (request, response) => {
-  try {
-    response.send("add");
-  } catch (err) {
-    if (err) {
-      return response.status(400).send(err);
-    } else {
-      return next(err);
-    }
-  }
+// router.use("/add", (request, response) => {
+//   response.send("add");
+// });
+
+// router.use("/subtract", (request, response) => {
+//   response.send("subtract");
+// });
+
+// router.use("/multiply", (request, response) => {
+//   response.send("multiply");
+// });
+
+// router.use("/divide", (request, response) => {
+//   response.send("divide");
+// });
+
+router.get("/add", (request, response) => {
+  const sum = Object.values(request.query)
+    .reduce((accumulator, currentValue) => accumulator.concat(currentValue), [])
+    .map((param) => Number(param))
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  response.send(`${sum}`);
 });
 
-router.use("/subtract", (request, response) => {
-  try {
-    response.send("subtract");
-  } catch (err) {
-    if (err) {
-      return response.status(400).send(err);
-    } else {
-      return next(err);
-    }
-  }
+router.get("/subtract", (request, response) => {
+  const difference = Object.values(request.query)
+    .reduce((accumulator, currentValue) => accumulator.concat(currentValue), [])
+    .map((param) => Number(param))
+    .reduce((previous, current) => previous - current);
+  response.send(`${difference}`);
 });
 
-router.use("/multiply", (request, response) => {
-  try {
-    response.send("multiply");
-  } catch (err) {
-    if (err) {
-      return response.status(400).send(err);
-    } else {
-      return next(err);
-    }
-  }
+router.get("/multiply", (request, response) => {
+  const product = Object.values(request.query)
+    .reduce((accumulator, currentValue) => accumulator.concat(currentValue), [])
+    .map((param) => Number(param))
+    .reduce((previous, current) => previous * current);
+  response.send(`${product}`);
 });
 
-router.use("/divide", (request, response) => {
-  try {
-    response.send("divide");
-  } catch (err) {
-    if (err) {
-      return response.status(400).send(err);
-    } else {
-      return next(err);
-    }
-  }
+router.get("/divide", (request, response) => {
+  const quotient = Object.values(request.query)
+    .reduce((accumulator, currentValue) => accumulator.concat(currentValue), [])
+    .map((param) => Number(param))
+    .reduce((previous, current) => previous / current);
+  response.send(`${quotient}`);
 });
+
+router.post("/add", (request, response) => {
+  console.log(request.body);
+  // const sum = Object.values(request.body)
+  //     .reduce((accumulator, currentValue) => accumulator.concat(currentValue), [])
+  //     .map((param) => Number(param))
+  //     .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  // response.send(`${sum}`);
+  response.send("add");
+});
+
+//   router.post("/subtract", (request, response) => {
+//     const difference = ;
+//     response.send(`${difference}`);
+//   });
+
+//   router.post("/multiply", (request, response) => {
+//     const product = ;
+//     response.send(`${product}`);
+//   });
+
+//   router.post("/divide", (request, response) => {
+//     const quotient = ;
+//     response.send(`${quotient}`);
+//   });
 
 app.use("/calculator", router);
 
-app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
+app.listen(3000, () => console.log("Calculator:listening on port 3000"));
