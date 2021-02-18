20be6fa885b42ff370344eae9bd72caca916bc76
Beth Jackson <beth.jack@gmail.com>
2021-02-18 10:10:07 +0100

    got router sorted for warmup

--- nodejs/week3/warmup/index.js
+++ nodejs/week3/warmup/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const router = express.Router();
 
-app.get("/", (req, res) => res.send("nodejs week3 homework"));
+router.use("/", (req, res) => res.send("nodejs week3 homework"));
+
+app.use("/calculator", router);
 
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
