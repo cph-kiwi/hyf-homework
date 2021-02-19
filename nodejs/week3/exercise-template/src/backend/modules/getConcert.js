55c3c4372069e905a6bde9187589e15bde602621
Beth Jackson <beth.jack@gmail.com>
2021-02-19 14:28:28 +0100

    Update

--- nodejs/week3/exercise-template/src/backend/modules/getConcert.js
+++ nodejs/week3/exercise-template/src/backend/modules/getConcert.js
@@ -1,10 +1,10 @@
 const assert = require("assert");
 const knex = require("../database");
 
-const concertAtId = async (id) => {
-  const concertAtId = await knex("concerts").where({ id: id });
-  assert.strictEqual(concertAtId.length < 2, true);
-  return concertAtId[0];
+const concertById = async (id) => {
+  const concertById = await knex("concerts").where({ id: id });
+  assert.strictEqual(concertById.length < 2, true);
+  return concertById[0];
 };
 
-module.exports = concertAtId;
+module.exports = concertById;
