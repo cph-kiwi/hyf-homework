35577f7a507ff19f8167bd9689e2df5d0e9d8d4a
Beth Jackson <beth.jack@gmail.com>
2021-02-22 13:04:23 +0100

    Finished hw without CSS

--- react/week1/to-do/src/index.js
+++ react/week1/to-do/src/index.js
@@ -1,80 +1,104 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-function ToDoItem({ task, description, datetime, deadline }) {
+// <TodoTable>
+//    <HeadingBar />
+//    <ListItemsTable>
+//        <ItemTitle />
+//        <ItemDetails />
+//    </ListItemsTable>
+// </TodoTable>
+
+function HeadingBar() {
+  return <h1>Beth's to-do list</h1>;
+}
+
+function ItemTitle({ title }) {
+  return <h2>{title}</h2>;
+}
+
+function ItemDetails({ title, task, dateCreated, deadline }) {
   return (
-    <>
-      <li>
-        <h3>Task: {task}</h3>
-        <h3>Description: {description}</h3>
-        <h3>Datetime: {datetime}</h3>
-        <h3>Deadline: {deadline}</h3>
-      </li>
-    </>
+    <li>
+      <ItemTitle title={title} />
+      <p>Task: {task}</p>
+      <p>Date created: {dateCreated}</p>
+      <p>Deadline: {deadline}</p>
+    </li>
   );
 }
 
-function ToDoList(props) {
+function ListItemsTable(props) {
   return (
-    <ul>
-      {props.toDoItems.map((item) => {
-        return (
-          <ToDoItem
-            task={item.task}
-            description={item.description}
-            datetime={item.datetime}
-            deadline={item.deadline}
-          />
-        );
-      })}
-    </ul>
+    <div>
+      <ul>
+        {props.toDoItems.map((item) => {
+          return (
+            <li key={item.id}>
+              <ItemDetails {...item} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function ToDoTable(props) {
+  return (
+    <div>
+      <HeadingBar />
+      <ListItemsTable toDoItems={props.toDoItems} />
+    </div>
   );
 }
 
 const toDoItems = [
   {
-    task: "Get out of bed",
-    description:
-      "Wake up and get out of bed, straighten sheets, get dressed, and tidy up",
-    datetime: "Wed Sep 13 2017",
-    deadline: "06:30",
+    id: 1,
+    title: "Doctor",
+    task: "Book appointment for daughter regarding earache",
+    dateCreated: "Mon Feb 22 2021",
+    deadline: "Tues Feb 23 2021",
   },
   {
-    task: "Brush teeth",
-    description:
-      "Use bathroom, brush teeth, put on deoderant, brush hair, and wash face",
-    datetime: "Thu Sep 14 2017",
-    deadline: "06:45",
+    id: 2,
+    title: "Garden",
+    task: "Tidy up garden - remove weeds and mow lawn",
+    dateCreated: "Fri Feb 19 2021",
+    deadline: "Mon Feb 22 2021",
   },
   {
-    task: "Eat breakfast",
-    description: "Make breakfast for family and set table",
-    datetime: "Fri Sep 15 2017",
-    deadline: "07:15",
+    id: 3,
+    title: "Books",
+    task: "Return books to the library",
+    dateCreated: "Mon Feb 22 2021",
+    deadline: "Fri Feb 26 2021",
+  },
+  {
+    id: 4,
+    title: "Shoes",
+    task: "Order shoes for daughter online",
+    dateCreated: "Sun Feb 21 2021",
+    deadline: "Thu Feb 25 2021",
+  },
+  {
+    id: 5,
+    title: "Prescription",
+    task: "Collect prescription from pharmacy",
+    dateCreated: "Mon Feb 22 2021",
+    deadline: "Fri Feb 26 2021",
+  },
+  {
+    id: 6,
+    title: "Laundry",
+    task: "Wash woolens",
+    dateCreated: "Fri Feb 19 2021",
+    deadline: "Mon Feb 22 2021",
   },
 ];
 
 ReactDOM.render(
-  <ToDoList toDoItems={toDoItems} />,
+  <ToDoTable toDoItems={toDoItems} />,
   document.getElementById("root")
 );
-
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-*/
